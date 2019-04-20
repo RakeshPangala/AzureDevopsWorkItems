3e3a0d15af4389e6078adcb87f6551546a098f60
Sai Krishna <46664894+KrishReigns@users.noreply.github.com>
2019-04-19 23:30:08 -0700

    Add files via upload
    Added azure Login Automation script.

---
+++ Sai Krishna/AzureLoginAutomation.ps1
@@ -0,0 +1,10 @@
+﻿#To store password in an encrypted format use below cmd
+read-host -assecurestring | convertfrom-securestring | out-file C:\cred.txt
+#create a username and password variables
+$userId = "applicationId@azuredomain.onmicrosoft.com"
+#Path where the password is stored in
+$password = get-content -Path "C:\cred.txt" | ConvertTo-SecureString
+#Set the powershell credential object
+$cred = New-Object -TypeName System.Management.Automation.PSCredential($userId ,$password)
+#log On To Azure Account
+Login-AzureRmAccount -Credential $cred -TenantId "Noted Tenant ID"
