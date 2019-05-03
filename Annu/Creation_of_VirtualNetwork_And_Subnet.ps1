f5af04f090a6a849a338b24589d4df0636ce0f5c
AnnuCh <46590008+AnnuCh@users.noreply.github.com>
2019-05-03 12:10:46 +0530

    Add files via upload

---
+++ Annu/Creation_of_VirtualNetwork_And_Subnet.ps1
@@ -0,0 +1,22 @@
+﻿Get-ExecutionPolicy
+
+#login to Azure portal
+
+Login-AzureRmAccount
+
+#creating Resource Group
+
+$rgName='AnnuRG'
+$location='eastus'
+New-AzureRmResourceGroup -Name $rgName -Location $location 
+
+#Creating Virtual Network
+
+$VN=New-AzureRmVirtualNetwork -ResourceGroupName "AnnuRG" -Location "East Us" -Name "AnnuVNet" -AddressPrefix "198.162.1.0/25"
+
+#Creating Subnets
+
+$SubnetConfig1=Add-AzureRmVirtualNetworkSubnetConfig -Name Frontendsubnet -AddressPrefix 198.162.1.0/27 -VirtualNetwork $VN
+$SubnetConfig2=Add-AzureRmVirtualNetworkSubnetConfig -Name Backendsubnet  -AddressPrefix 198.162.1.32/27 -VirtualNetwork $VN
+
+$VN | Set-AzureRmVirtualNetwork
