01688a0c5d4938958e036b69abbfe3ea0e02d1dd
AnnuCh <46590008+AnnuCh@users.noreply.github.com>
2019-05-06 18:05:51 +0530

    Add files via upload

---
+++ Annu/Creation_of_VM_ScaleSet.ps1
@@ -0,0 +1,19 @@
+﻿Login-AzureRmAccount
+$rgname='AnnuRG'
+$loc='East US'
+
+#----Creating Resource group-------
+
+New-AzureRmResourceGroup -Name $rgname  -Location $loc
+
+#----Creating Virtual Network-------
+
+$vNet=New-AzureRmVirtualNetwork  -ResourceGroupName $rgname -Location $loc -Name anuVnet -AddressPrefix 10.0.0.0/24
+
+#-------Creating Subnet---------
+
+New-azureRmVirtualNetworksubnetconfig -Name Anusubnet -AddressPrefix 10.0.0.0/28 
+
+#Creating Virtual machine Scale set
+
+New-AzureRmVmss -ResourceGroupName $rgname -Location $loc -VMScaleSetName "AnnuScaleSet"  -VirtualNetworkName $vNet -SubnetName "Anusubnet" -PublicIpAddressName "annuPublicIPAddress" -LoadBalancerName "annuLoadBalancer" -UpgradePolicyMode "Automatic"
