f5aaa38ae2c57ffe952ef98c6a8bc86075c7d396
VaijayanthiS <46589969+VaijayanthiS@users.noreply.github.com>
2019-05-06 23:47:28 +0530

    Add files via upload

---
+++ Vaijayanthi/VMCreation.ps1
@@ -0,0 +1,12 @@
+﻿$ResourceGroupName='VaijayanthiRG'
+$VirtualMachineName='VaijayanthiVM'
+$Location='Central US'
+$VirtualNetworkName='VaijayanthiVnet'
+$SubnetName='VaijayanthiSubnet'
+$SecurityGroupName='VaijayanthiNSG'
+$PublicIpAddressName='VaijayanthiPIP'
+
+New-AzureRmVm -ResourceGroupName $VaijayanthiRG -Name $VirtualMachineName -Location $Location -VirtualNetworkName $VirtualNetworkName -SubnetName $SubnetName -SecurityGroupName $SecurityGroupName -PublicIpAddressName $PublicIpAddressName -OpenPorts 80,3389
+
+    #To Connect Virtual Machine
+    Get-AzPublicIpAddress -ResourceGroupName $VaijayanthiRG | Select $PublicIpAddressName
