794f09128ffb91879298054f93fb2521fd27e4a6
AnnuCh <46590008+AnnuCh@users.noreply.github.com>
2019-05-06 19:15:56 +0530

    Add files via upload

---
+++ Annu/Recovery_and_backup_of_VM.ps1
@@ -0,0 +1,42 @@
+﻿#----Recovery and Backup of VM
+
+Login-AzureRmAccount
+$rgname='AnnuRG'
+$loc='East US'
+
+#----Creating Resource group-------
+
+New-AzureRmResourceGroup -Name $rgname  -Location $loc
+
+#----Creating virtual network--------
+
+New-AzureRmVm -ResourceGroupName $rgname -Location $loc -Name AnnuVM
+
+#----Creating Recovery Services Vault-------- 
+
+    New-AzureRmRecoveryServicesVault -Name "AnnuRSG" -ResourceGroupName $rgname -Location $loc
+
+    $vault= Get-AzureRmRecoveryServicesVault -Name "AnnuRSG"
+
+   Set-AzureRmRecoveryServicesBackupProperties -Vault $vault -BackupStorageRedundancy GeoRedundant
+
+   #Back up Azure VMs
+
+   #Before enabling protection on a VM, uses Set-AzRecoveryServicesVaultContext to set the vault context. 
+
+$vault | Set-AzureRmRecoveryServicesVaultContext
+
+#Create a protection policy
+
+Get-AzureRmRecoveryServicesBackupProtectionPolicy -WorkloadType "AzureVM"
+
+$schPol = Get-AzureRmRecoveryServicesBackupSchedulePolicyObject -WorkloadType "AzureVM"
+$UtcTime = Get-Date -Date "2019-05-23 01:00:00Z"
+$UtcTime = $UtcTime.ToUniversalTime()
+$schpol.ScheduleRunTimes[0] = $UtcTime
+
+$retPol = Get-AzureRmRecoveryServicesBackupRetentionPolicyObject -WorkloadType "AzureVM"
+
+#creating Recovery Services Backup Protection Policy
+
+New-AzureRmRecoveryServicesBackupProtectionPolicy -Name "AnnuPolicy" -WorkloadType "AzureVM" -RetentionPolicy $retPol -SchedulePolicy $schPol
