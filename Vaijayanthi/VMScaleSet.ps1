b40fa80b7d0b2c07176e5a40e0b2236accba9a2d
VaijayanthiS <46589969+VaijayanthiS@users.noreply.github.com>
2019-05-06 23:51:07 +0530

    Add files via upload

---
+++ Vaijayanthi/VMScaleSet.ps1
@@ -0,0 +1,28 @@
+﻿
+Login-AzureRmAccount
+
+#Required fields--------------------
+$RGnNme='VaijayanthiRG'
+$Location='Central US'
+$VMScaleSetName='VaijayanthiSSet'
+$SubnetName='FrontendSubnet'
+$PublicIpAddressName='VaijayanthiPIP'
+$LoadBalancerName='VaijayanthiLB'
+$UpgradePolicyMode='Automatic'
+$virtualNetworkName='Vaijayanthi-Vnet'
+
+#Creating Resource group-----------------
+
+New-AzureRmResourceGroup -Name $RGnNme  -Location $Location
+
+#Creating Virtual Network---------------------
+
+$VNet=New-AzureRmVirtualNetwork  -ResourceGroupName $RGnNme -Location $Location -Name $virtualNetworkName -AddressPrefix 10.0.0.0/24
+
+#Creating Subnet-----------------------
+
+New-azureRmVirtualNetworksubnetconfig -Name Anusubnet -AddressPrefix 10.0.0.0/28
+
+#Creating Virtual machine Scale set-------------------------------------
+
+New-AzureRmVmss -ResourceGroupName $RGnNme -Location $Location -VMScaleSetName $VMScaleSetName  -VirtualNetworkName $VNet -SubnetName $SubnetName -PublicIpAddressName $PublicIpAddressName -LoadBalancerName $LoadBalancerName  -UpgradePolicyMode $UpgradePolicyMode
