e6790577f00927b196649e29c25e1f00e301ebe4
B121215 <46589065+B121215@users.noreply.github.com>
2019-05-06 23:26:55 +0530

    Add files via upload
    Task3

---
+++ Gowthami/AzureTrainingTask3.ps1
@@ -0,0 +1,45 @@
+﻿
+Login-AzureRmAccount
+#creating Virtual machine
+New-AzureRmVm -ResourceGroupName GowthamiRG -Location "East Us" -Name "Gowthami-VM" 
+
+
+
+$resourceGroupName = 'GowthamiRG' 
+$location = 'eastus' 
+
+#Getting Azure virtual machine what we created previously
+get-azureRmVm -ResourceGroupName GowthamiRG -Name "Gowthami-VM"
+
+
+
+
+#Creating Snapshot for Adding into  vitual machine 
+
+New-AzureRmSnapshot -ResourceGroupName GowthamiRG -Snapshot GowthamiSnapshot -SnapshotName GowthamiSnapshot1
+
+#Creating Managed Disk in azure
+ConvertTo-AzureRmVMManagedDisk -ResourceGroupName GowthamiRG -VMName Gowthami-VM
+
+#New-AzureRmDisk -Disk GowthamiDisk -DiskName GowthamiDisk1 -ResourceGroupName GowthamiRG
+
+
+
+#Adding disk to Vm
+
+Add-AzureRmVMDataDisk -CreateOption VmDisj -Lun GLun -VM Gowthami-VM
+
+
+
+
+#Creating Imaging with vm
+New-AzureRmImage -Image Image -ImageName Gowthami-Image -ResourceGroupName GowthamiRG
+
+#Adding imaging 
+
+Add-AzureRmImageDataDisk -Image Gowthami-image 
+
+
+
+
+
