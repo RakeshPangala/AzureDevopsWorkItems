adbcaeddc4c34a4e452e3e9ba1213adf72babb3b
surya8899 <49714137+surya8899@users.noreply.github.com>
2019-05-07 16:55:36 +0530

    Add files via upload

---
+++ surya8899/Created Key Vault.ps1
@@ -0,0 +1,25 @@
+﻿# Create a Azure key vault
+
+# Input Area
+
+$subscriptionName='Pay-As-You-Go'
+$ResourceGroupName='SuryaRG'
+$Location='South india'
+$KeyVaultName='SuryaKEyVault'
+
+
+# login Azure Account
+
+Login-AzureRmAccount -SubscriptionName -$SubscriptionName
+
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $KeyVaultName
+
+
+#Create Azure Key Vault
+
+New-AzureRmKeyVault -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -Location $KeyVaultLocation -Sku 'Standard'
+
+
+#Permit the Azure Backup service to access the key vault
+
+Set-AzureRmKeyVaultAccessPolicy -VaultName $keyVaultName -ResourceGroupName $resourceGroupName -PermissionsToKeys 'WrapKey' -PermissionsToSecrets get,list,set -ServicePrincipalName VMEncryptionSvcPrincipalName
