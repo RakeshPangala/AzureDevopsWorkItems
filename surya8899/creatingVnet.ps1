676da42fd25f79fd1d22578920278b737a199e99
surya8899 <49714137+surya8899@users.noreply.github.com>
2019-05-07 16:53:55 +0530

    Add files via upload

---
+++ surya8899/creatingVnet.ps1
@@ -0,0 +1,33 @@
+﻿#$AzureLoginId=""
+#$AzurePassword=ConvertTo-SecureString "" -AsPlainText -Force
+#$AzureCreds= New-Object System.Management.Automation.PSCredential($AzureLoginId,$AzurePassword)
+#Login-AzureRmAccount -Credential $AzureCreds
+
+#Login to Azure portal with Credenials
+Login-AzureRmAccount
+
+#Creating Resource Group
+$ResourceGroupName="SuryaRG"
+$Location="South india"
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+
+#Fields required to create Vnet and subnets
+$VirtualNetworkName="SuryaVnet"
+$SubnetName1="FrontendSubnet"
+#$SubnetName2="BackendSubnet"
+$AddressSpace="10.3.0.0/24"
+$AddressRange1="10.3.0.0/28"
+$AddressRange2="10.3.0.32/28"
+
+#Creating Virtual Network
+$VirtualNetwork= New-AzureRmVirtualNetwork -ResourceGroupName $ResourceGroupName -Location $Location -Name $VirtualNetworkName -AddressPrefix $AddressSpace
+
+#Creating Subnet1
+$SubnetConfig1=Add-AzureRmVirtualNetworkSubnetConfig -Name $SubnetName1 -AddressPrefix $AddressRange1 -VirtualNetwork $VirtualNetwork
+
+$virtualNetwork | Set-AzureRmVirtualNetwork
+
+#Creating Subnet2
+$SubnetConfig2=Add-AzureRmVirtualNetworkSubnetConfig -Name $SubnetName2 -AddressPrefix $AddressRange2 -VirtualNetwork $VirtualNetwork
+
+$virtualNetwork | Set-AzureRmVirtualNetwork
