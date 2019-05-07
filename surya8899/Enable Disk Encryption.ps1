b802b045dcd3f5c30f4cb9d2f529a66175f3cb3d
surya8899 <49714137+surya8899@users.noreply.github.com>
2019-05-07 18:06:35 +0530

    Add files via upload

---
+++ surya8899/Enable Disk Encryption.ps1
@@ -0,0 +1,25 @@
+﻿# Enable Disk Encryption
+
+#Input Area
+$subscriptionName = 'Pay-As-You-Go'
+$resourceGroupName = 'SuryaRG'
+$keyVaultName = 'SuryaKeyVault'
+$keyName = 'VMEncryption-Suryakname'
+$aadSvcPrinAppDisplayName = 'VMEncryptionSvcPrincipal'
+$aadSvcPrinAppPassword = ConvertTo-SecureString '' -AsPlainText -Force
+$vmName = 'SuryaVm'
+
+
+#Manual login into Azure
+Login-AzureRmAccount -SubscriptionName $subscriptionName
+
+
+
+#Enable Encryption on Virtual Machine
+
+$keyVault = Get-AzureRmKeyVault -VaultName $keyVaultName -ResourceGroupName $resourceGroupName
+$diskEncryptionKeyVaultUrl = $KeyVault.VaultUri
+$keyVaultResourceId = $KeyVault.ResourceId
+$keyEncryptionKeyUri = Get-AzureKeyVaultKey -VaultName $keyVaultName -KeyName $keyName 
+$aadSvcPrinApplication = Get-AzureRmADApplication -DisplayName $aadSvcPrinAppDisplayName 
+Set-AzureRmVMDiskEncryptionExtension -ResourceGroupName $resourceGroupName -VMName $vmName -AadClientID $aadSvcPrinApplication.ApplicationId -AadClientSecret $aadSvcPrinAppPassword -DiskEncryptionKeyVaultUrl $diskEncryptionKeyVaultUrl -DiskEncryptionKeyVaultId $KeyVaultResourceId -KeyEncryptionKeyUrl $keyEncryptionKeyUri.Id -KeyEncryptionKeyVaultId $keyVaultResourceId
