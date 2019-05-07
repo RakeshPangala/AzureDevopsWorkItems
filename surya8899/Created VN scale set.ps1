485254414f9ca849ec840a1637470b798c9ae3fe
surya8899 <49714137+surya8899@users.noreply.github.com>
2019-05-07 22:25:24 +0530

    Add files via upload

---
+++ surya8899/Created VN scale set.ps1
@@ -0,0 +1,19 @@
+﻿Login-AzureRmAccount
+$rgname='SuryaRG'
+$loc='South india'
+
+#----Creating Resource group-------
+
+New-AzureRmResourceGroup -Name $rgname  -Location $loc
+
+#----Creating Virtual Network-------
+
+$vNet=New-AzureRmVirtualNetwork  -ResourceGroupName $rgname -Location $loc -Name SuryaVnet -AddressPrefix 10.0.0.0/24
+
+#-------Creating Subnet---------
+
+New-azureRmVirtualNetworksubnetconfig -Name Suryasubnet -AddressPrefix 10.0.0.0/28 
+
+#Creating Virtual machine Scale set
+
+New-AzureRmVmss -ResourceGroupName $rgname -Location $loc -VMScaleSetName "SuryaScaleSet"  -VirtualNetworkName $vNet -SubnetName "Suryasubnet" -PublicIpAddressName "SuryaPublicIPAddress" -LoadBalancerName "SuryaLoadBalancer" -UpgradePolicyMode "Automatic"
