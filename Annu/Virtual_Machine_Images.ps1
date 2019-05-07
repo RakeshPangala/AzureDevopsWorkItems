7e6e0784d3eb6ab80475d3699d6398b93e24144c
AnnuCh <46590008+AnnuCh@users.noreply.github.com>
2019-05-07 18:21:36 +0530

    Add files via upload

---
+++ Annu/Virtual_Machine_Images.ps1
@@ -0,0 +1,29 @@
+﻿
+
+Login-AzureRmAccount
+
+$VmName = "AnnuVm"
+
+$rgName = "AnnuRG"
+
+$loc = "East US"
+
+$ImageName = "AnnuRG" 
+
+#----------------Creating Resource Group------------------------
+
+New-AzureRmResourceGroup -Name $rgName  -Location $loc
+
+#----------------Creating Virtual Network------------------------
+
+$Vm=New-AzureRmVm -ResourceGroupName $rgName -Location $loc -Name $VmName
+
+#--------------Stoping the virtual Network------------------------
+
+Stop-AzureRmVM -ResourceGroupName $rgName -Name $VmName -Force
+
+Set-AzureRmVm -ResourceGroupName $rgName -Name $VmName -Generalized
+
+$vm = Get-AzureRmVM -Name $vmName -ResourceGroupName $rgName
+
+$image = New-AzureRmImageConfig -Location $location -SourceVirtualMachineId $vm.IdNew-AzureRmImage -Image $image -ImageName $imageName -ResourceGroupName $rgName
