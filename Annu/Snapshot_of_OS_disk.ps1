5c7fc5c2974d8976e9e9a0525443b579fb6e81bd
AnnuCh <46590008+AnnuCh@users.noreply.github.com>
2019-05-07 18:22:10 +0530

    Add files via upload

---
+++ Annu/Snapshot_of_OS_disk.ps1
@@ -0,0 +1,19 @@
+﻿
+#Creating Resource Group
+
+$RG=New-AzureRmResourceGroup -Name $rgName -Location  $loc
+
+#Creating Virtual Machine
+
+$Vm=New-AzureRmVm -ResourceGroupName $rgName -Location $loc -Name $VmName
+
+#Getting Vm detailes here
+
+$VmDetailes = Get-azureRmVm -ResourceGroupName $rgName -Name $VmName
+
+#creating snapshot configure
+
+$snapshot =  New-AzureRmSnapshotConfig -SourceUri $Vm.StorageProfile.OsDisk.ManagedDisk.Id -Location $loc -CreateOption copy
+
+#Creating Snapshot
+New-AzureRmSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $rgName
