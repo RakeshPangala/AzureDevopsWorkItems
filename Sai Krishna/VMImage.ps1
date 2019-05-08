649cb4409ae1c0e1a2f1c8397564d34e9dec5df5
Sai Krishna <46664894+KrishReigns@users.noreply.github.com>
2019-05-08 05:49:25 -0700

    Add files via upload
    Adding VmImage File

---
+++ Sai Krishna/VMImage.ps1
@@ -0,0 +1,19 @@
+﻿Login-AzureRmAccount
+
+$VMName = "KrishVM"
+$RGName = "KrishRG"
+$Loc = "South India"
+$imageName = "KrishImage"
+
+
+Stop-AzureRmVM -ResourceGroupName $RGName -Name $VMName -Force 
+
+
+
+Set-AzureRmVm -ResourceGroupName $RGName -Name $VMName -Generalized
+
+$vm = Get-AzureRmVM -Name $VMName -ResourceGroupName $RGName
+
+$image = New-AzureRmImageConfig -Location $Loc -SourceVirtualMachineId $vm.Id
+
+New-AzureRmImage -Image $image -ImageName $imageName -ResourceGroupName $RGName
