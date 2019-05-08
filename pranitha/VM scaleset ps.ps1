2e8f501a0b3f147fe63aa2728541a81a41571dc9
pranitha112 <49674063+pranitha112@users.noreply.github.com>
2019-05-07 20:35:19 -0700

    Add files via upload

---
+++ pranitha/VM scaleset ps.ps1
@@ -0,0 +1,27 @@
+﻿Login-AzureRmAccount
+
+#Required fields--------------------
+$RGnNme='PranithaRG'
+$Location='Central US'
+$VMScaleSetName='PranithaSSet'
+$SubnetName='FrontendSubnet'
+$PublicIpAddressName='PranithaPIP'
+$LoadBalancerName='PranithaLB'
+$UpgradePolicyMode='Automatic'
+$virtualNetworkName='Pranitha-Vnet'
+
+#Creating Resource group-----------------
+
+New-AzureRmResourceGroup -Name $RGnNme  -Location $Location
+
+#Creating Virtual Network---------------------
+
+$VNet=New-AzureRmVirtualNetwork  -ResourceGroupName $RGnNme -Location $Location -Name $virtualNetworkName -AddressPrefix 10.0.0.0/24
+
+#Creating Subnet-----------------------
+
+New-azureRmVirtualNetworksubnetconfig -Name Anusubnet -AddressPrefix 10.0.0.0/28
+
+#Creating Virtual machine Scale set-------------------------------------
+
+New-AzureRmVmss -ResourceGroupName $RGnNme -Location $Location -VMScaleSetName $VMScaleSetName  -VirtualNetworkName $VNet -SubnetName $SubnetName -PublicIpAddressName $PublicIpAddressName -LoadBalancerName $LoadBalancerName  -UpgradePolicyMode $UpgradePolicyMode
