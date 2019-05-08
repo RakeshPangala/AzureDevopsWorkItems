ed6a4d6cedcf2a11b69a9edd722ad5f8bc51c9e4
RakeshPangala <46561538+RakeshPangala@users.noreply.github.com>
2019-05-08 19:29:32 +0530

    Add files via upload

---
+++ Rakesh/snapshot.ps1
@@ -0,0 +1,43 @@
+﻿#Parameters
+
+$ResourceGroupName='RakeshRG'
+
+$Location='South India'
+
+$snapshotName = 'RakeshSnapshot'
+
+$vmName='RakeshVM'
+
+
+
+
+
+#Creating Resource group-----------------
+
+New-AzureRmResourceGroup -Name $resourceGroupName  -Location $location
+
+
+
+#Creating VM
+
+$Vmdetails=New-AzureRmVm -ResourceGroupName $ResourceGroupName -Location $Location -Name $vmName
+
+
+
+#Get the VM
+
+$vm = Get-AzureRmVm -ResourceGroupName $ResourceGroupName -Name $vmName
+
+
+
+#Create the snapshot configuration. For this example, the snapshot is of the OS disk
+
+$snapshot =  New-AzureRmSnapshotConfig -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id -Location $Location -CreateOption copy
+
+
+
+#Take the snapshot:
+
+
+
+New-AzureRmSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $ResourceGroupName 
