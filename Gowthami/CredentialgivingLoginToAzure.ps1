17d3e28ce4cd7ea257ad53785cd926457124b135
ranjitbalu <ranjit.balu@gmail.com>
2019-05-08 10:30:48 +0530

    Update CredentialgivingLoginToAzure.ps1

--- Gowthami/CredentialgivingLoginToAzure.ps1
+++ Gowthami/CredentialgivingLoginToAzure.ps1
@@ -8,7 +8,11 @@ Login-AzureRmAccount -Credential $Credential
 #Login-AzureRmAccount -Credential $Credential -TenantId "Noted Tenant ID"
 #Connect-AzureRmAccount -Credential $Credential
    
+$AzureAcct = "rkumar@osibbsi.onmicrosoft.com"
+$AzurePwd = ConvertTo-SecureString "XXXX" -AsPlainText -Force
+$AzureCreds = New-Object System.Management.Automation.PSCredential($AzureAcct, $AzurePwd)
 
+Login-AzureRmAccount -Credential $AzureCreds -TenantId "d38d2c2c-d50c-46ca-9ad4-1cda22e3036b" -SubscriptionId "fab4f174-2c82-4030-a8c3-3848c2d97f2f"
 
 
 #Otherer way to giving a credential
