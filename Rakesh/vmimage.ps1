0dccfb6365b5d144e6573e0edfc9e0d76e5b68ff
RakeshPangala <46561538+RakeshPangala@users.noreply.github.com>
2019-05-08 19:30:48 +0530

    Add files via upload

---
+++ Rakesh/vmimage.ps1
@@ -0,0 +1,32 @@
+﻿Login-AzureRmAccount
+
+
+
+$VMName = "RakeshVM"
+
+$RGName = "RakeshRG"
+
+$Loc = "South India"
+
+$imageName = "RakeshImage"
+
+
+
+
+
+Stop-AzureRmVM -ResourceGroupName $RGName -Name $VMName -Force 
+
+
+Set-AzureRmVm -ResourceGroupName $RGName -Name $VMName -Generalized
+
+
+
+$vm = Get-AzureRmVM -Name $VMName -ResourceGroupName $RGName
+
+
+
+$image = New-AzureRmImageConfig -Location $Loc -SourceVirtualMachineId $vm.Id
+
+
+
+New-AzureRmImage -Image $image -ImageName $imageName -ResourceGroupName $RGName
