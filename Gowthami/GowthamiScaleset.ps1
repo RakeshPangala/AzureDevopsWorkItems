ab80491b942e68907c6b1e9558c65fa8e9c28ddc
B121215 <46589065+B121215@users.noreply.github.com>
2019-05-08 06:40:07 +0530

    Add files via upload
    Uploding Task4

---
+++ Gowthami/GowthamiScaleset.ps1
@@ -0,0 +1,19 @@
+﻿Login-AzureRmAccount
+$rgname='GowthamiRG'
+$loc='East US'
+
+#----Creating Resource group-------
+
+New-AzureRmResourceGroup -Name $rgname  -Location $loc
+
+#----Creating Virtual Network-------
+
+$vNet=New-AzureRmVirtualNetwork  -ResourceGroupName $rgname -Location $loc -Name GowthamiVnet -AddressPrefix 198.162.0.0/24
+
+#-------Creating Subnet---------
+
+New-azureRmVirtualNetworksubnetconfig -Name Gowthamisubnet -AddressPrefix 198.162.0.0/28
+
+#Creating Virtual machine Scale set
+
+New-AzureRmVmss -ResourceGroupName $rgname -Location $loc -VMScaleSetName "GowthamiScaleSet"  -VirtualNetworkName $vNet -SubnetName "Gowthamisubnet" -PublicIpAddressName "GowthamiPublicIPAddress" -LoadBalancerName "GowthamiLoadBalancer" -UpgradePolicyMode "Automatic"
