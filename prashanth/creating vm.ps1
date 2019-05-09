13a5485e38d8da874d5c31c2163846db7691752b
kPrashanth007 <49711439+kPrashanth007@users.noreply.github.com>
2019-05-09 06:31:38 +0530

    Add files via upload

---
+++ prashanth/creating vm.ps1
@@ -0,0 +1,23 @@
+﻿$ResourceGroupName='kprg'
+
+$VirtualMachineName='kpvn'
+
+$Location='south india'
+
+$VirtualNetworkName='kpVnet'
+
+$SubnetName='kpSubnet'
+
+$SecurityGroupName='kpNSG'
+
+$PublicIpAddressName='kpPIP'
+
+
+
+New-AzureRmVm -ResourceGroupName $kprg -Name $VirtualMachineName -Location $Location -VirtualNetworkName $VirtualNetworkName -SubnetName $SubnetName -SecurityGroupName $SecurityGroupName -PublicIpAddressName $PublicIpAddressName -OpenPorts 80,3389
+
+
+
+    #To Connect Virtual Machine
+
+    Get-AzPublicIpAddress -ResourceGroupName $kprg | Select $PublicIpAddressName
