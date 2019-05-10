182f5d96c31209c057eee75417fcea17ba22126d
kumarreddyk <49679119+kumarreddyk@users.noreply.github.com>
2019-05-10 03:30:14 -0700

    Add files via upload

---
+++ Kumar/Azure task4.ps scaleset.ps1
@@ -0,0 +1,37 @@
+﻿Login-AzureRmAccount
+
+#Required fields--------------------
+
+$ResourceGroup='kumar'
+
+$Location='south india'
+
+$VMScaleSetName='kumarSSet'
+
+$SubnetName='FrontendSubnet'
+
+$PublicIpAddressName='kumarPIP'
+
+$LoadBalancerName='KumarLB'
+
+$UpgradePolicyMode='Automatic'
+
+$virtualNetworkName='kumar-Vnet'
+
+
+
+#Creating Resource group-----------------
+
+New-AzureRmResourceGroup -Name $ResourceGroup -Location $Location
+
+#Creating Virtual Network---------------------
+
+$VNet=New-AzureRmVirtualNetwork  -ResourceGroupName $ResourceGroup -Location $Location -Name $virtualNetworkName -AddressPrefix 190.168.0.0/24
+
+#Creating Subnet-----------------------
+
+New-azureRmVirtualNetworksubnetconfig -Name $SubnetName -AddressPrefix 190.168.0.0/28
+
+#Creating Virtual machine Scale set-------------------------------------
+
+New-AzureRmVmss -ResourceGroupName $ResourceGroup -Location $Location -VMScaleSetNam
