4846e0839414381845e37ce2ff16b15647dbda8e
B121215 <46589065+B121215@users.noreply.github.com>
2019-05-13 22:49:25 +0530

    Add files via upload
    Powershell script for creating storage account

---
+++ Gowthami/StorageAccount.ps1
@@ -0,0 +1,24 @@
+﻿
+
+#Login into the azure portal
+Login-AzAccount
+# Get list of locations and select one.
+Get-AzLocation | select Location
+$location = "eastus"
+
+# Create a new resource group.
+$resourceGroup = "GowthamiRG"
+New-AzResourceGroup -Name $resourceGroup -Location $location
+
+# Set the name of the storage account and the SKU name.
+$storageAccountName = "gowthamistorageaccount"
+$skuName = "Standard_LRS"
+
+# Create the storage account.
+$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroup `
+  -Name $storageAccountName `
+  -Location $location `
+  -SkuName $skuName
+
+# Retrieve the context.
+$ctx = $storageAccount.Context
