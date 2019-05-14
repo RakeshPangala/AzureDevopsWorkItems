26fb87746816713b4cabf499082c9677f5947e7d
Sai Krishna <46664894+KrishReigns@users.noreply.github.com>
2019-05-14 22:25:48 +0530

    Storage account

---
+++ Sai Krishna/StorageAccount.ps1
@@ -0,0 +1,60 @@
+﻿Login-AzureRmAccount
+#----------------------------------------------------------------------------------------------------
+#input Variables
+
+$resourceGroup = "KrishRG"
+
+$Location="South India"
+
+$storageAccountName = "krishnatorage"
+
+$skuName = "Standard_LRS"
+
+$Kind="BlobStorage"
+
+$AccessTier="Hot"
+
+$ContainerName="krishcontainer"
+
+#--------------------------------------------------------------------------------------------------
+# Resource group creation
+
+New-AzureRmResourceGroup -NAME $resourceGroup -Location $Location
+
+
+
+#Creating a Blob Storage account with BlobStorage Kind and selecting its AccessTier also get the context
+
+$storageAccount = New-AzureRmStorageAccount -ResourceGroupName $resourceGroup `
+ -AccountName $storageAccountName -Location $Location -SkuName $skuName -Kind $Kind -AccessTier $AccessTier
+
+
+$context = $storageAccount.Context
+#----------------------------------------------------------------------------------------------------
+
+#Creating  a storage container.
+
+$Container=New-AzureStorageContainer -Name $ContainerName -Context $context -Permission Off
+
+
+
+# upload a file
+
+set-AzureStorageblobcontent -File "C:\Images" `
+
+  -Container $ContainerName `
+
+  -Blob "Bestscene.jpg" `
+
+  -Context $context
+#-----------------------------------------------------------------------------------------------------
+
+#download blob
+
+Get-AzureStorageblobcontent -Blob "Bestscene.png" `
+
+  -Container $containerName `
+
+  -Destination "C:\Users\Sai" `
+
+  -Context $context
