b43df0a5962025b6d9357b45b538cd1950c7b54a
Sanjay <46832361+Sanjay3761@users.noreply.github.com>
2019-05-14 17:56:14 +0530

    Add files via upload

---
+++ Sanjay/Creating Storage Accounts, Containers, Uploading files.ps1
@@ -0,0 +1,25 @@
+﻿Login-AzAccount
+
+
+#----------------------------------Creates a Resource Group--------------------------------#
+
+$resourceGroupName= New-AzResourceGroup -Name Sanjay -Location 'South India'
+
+#----------------------------------Creates a Storage Account-------------------------------#
+
+$resourceGroupName='Sanjay'
+
+
+$storageAccountName= New-AzStorageAccount -ResourceGroupName Sanjay -Name sanjaystorageacc -SkuName Standard_LRS -Location 'south india' -Kind StorageV2 -AccessTier Hot
+
+#----------------------------------Creates Context----------------------------------------#
+
+$context = $storageAccountName.Context
+
+#----------------------------------Creates a Storage Container------------------------------#
+
+$storageContainer = New-AzStorageContainer -Name sanjaycontainer -Permission Container -Context $context
+
+#-----------------------------------Uploading an image in Container-------------------------#
+
+Set-AzStorageBlobContent -File "C:\Users\SANJAY\Desktop\minions.jpg" -Container "sanjaycontainer" -Blob "minions.jpg" -Context $context
