b8f304cb723b9f619a38cf5daeda1731f5c91145
pranitha112 <49674063+pranitha112@users.noreply.github.com>
2019-05-14 00:45:10 -0700

    Add files via upload

---
+++ pranitha/powershellscript for storageaccount.ps1
@@ -0,0 +1,17 @@
+﻿Get-ExecutionPolicy
+Install-Module -Name Az -AllowClobber
+Import-Module -Name Az
+Connect-AzAccount
+Login-AzAccount
+New-AzResourceGroup -Name pranithaRG3 -Location centralus
+Set the name of the storage account and the SKU name.
+$storageAccountName = "storageaccount1"
+$skuName = "Standard_LRS"
+
+ Create the storage account
+$storageAccount = New-AzStorageAccount -ResourceGroupName pranithaRG3
+  -Name $storageAccountName `
+  -Location $location `
+  -SkuName $skuName
+ Retrieve the context
+$ctx = $storageAccount.Context
