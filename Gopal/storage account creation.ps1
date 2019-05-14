17df554c4e31735abdf4a0e6ad9b7c6c8dc7531f
GOPALRAO1 <49671757+GOPALRAO1@users.noreply.github.com>
2019-05-14 17:25:29 +0530

    Add files via upload

---
+++ Gopal/storage account creation.ps1
@@ -0,0 +1,33 @@
+﻿Set-ExecutionPolicy RemoteSigned
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+Get-ExecutionPolicy
+Import-Module AzureRm
+Install-Module AzureRm
+Login-AzureRmAccount
+
+
+
+#Install-Module -Name Az -AllowClobber
+
+#login Azure portal
+
+$rg = "gopal"
+$loc = "central us"
+
+#create a resource group
+
+New-AzResourceGroup -Name $rg -Location $loc
+ 
+# create a storage account
+
+$storageacc = New-AzStorageAccount -ResourceGroupName $rg -Name "gopalstorageaccount" -SkuName "Standard_LRS" -Location $loc 
+
+$ctx = $storageacc.Context
+
+#create blob container
+
+New-AzStorageContainer -Name "gopalblobcontainer" -Permission "Blob" -Context $ctx
+
+# uplode the .jpg file
+
+Set-AzStorageBlobContent -File "Desktop\car123.jpg" -Container "gopalblobcontainer" -Blob "car123.jpg" -Context $ctx
