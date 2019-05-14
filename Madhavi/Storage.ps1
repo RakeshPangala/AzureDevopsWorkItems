830bf5c58a264aed72d530faf47a68311cd07834
MadhaviKonthalapally <48505956+MadhaviKonthalapally@users.noreply.github.com>
2019-05-14 23:25:48 +0530

    Add files via upload

---
+++ Madhavi/Storage.ps1
@@ -0,0 +1,17 @@
+﻿
+Login-AzureRmAccount
+
+New-AzureRmResourceGroup -Name MadhuRG -Location "East US"
+
+$storageAcc=New-AzureRmStorageAccount -ResourceGroupName MadhuRG -Name madhustorage958acc -SkuName Standard_LRS -Location "East US" -AccessTier Hot -Kind BlobStorage 
+
+$ctx=$storageAcc.Context
+
+New-AzureStorageContainer -Name ”madhavicontainer” -Context $ctx -Permission Off
+
+
+Set-AzureStorageBlobContent -Container ”madhavicontainer” -File "C:\Users\v-makont\Desktop\Capture" -Context $ctx
+
+$blobs = Get-AzureStorageBlob -Container ”madhavicontainer” -Context $ctx
+
+
