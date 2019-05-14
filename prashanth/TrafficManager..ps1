2752573e72752e68cac6a96ac146d12ef0e6cc5b
kPrashanth007 <49711439+kPrashanth007@users.noreply.github.com>
2019-05-14 07:33:13 +0530

    Add files via upload

---
+++ prashanth/TrafficManager..ps1
@@ -0,0 +1,90 @@
+﻿Login-AzureRmAccount
+
+$rgname='kprg'
+
+$loc='East US'
+
+#----Creating Resource group-------
+
+
+
+New-AzureRmResourceGroup -Name $rgname  -Location $loc
+
+#------------Creating traffic manager profile having Routing Method as Performance---------------------------------
+
+
+
+$Tmprofile = New-AzureRmTrafficManagerProfile -Name kpTMPer -ResourceGroupName $rgname -TrafficRoutingMethod Performance -RelativeDnsName kpDns -Ttl 30 -MonitorProtocol HTTP -MonitorPort 80 -MonitorPath "/"
+
+
+
+#------------Retrieve an existing Traffic Manager profile object
+
+
+
+$TmProfile = Get-AzureRmTrafficManagerProfile -Name kpTMPer -ResourceGroupName $rgname
+
+#------------Adding endpoints in Traffic Manager Profile having endpoint location as North-europe------------------------------------
+
+
+
+Add-AzureRmTrafficManagerEndpointConfig -EndpointName eu-endpoint -TrafficManagerProfile $Tmprofile -Type ExternalEndpoints -Target app-eu.contoso.com -EndpointLocation "North Europe" -EndpointStatus Enabled
+
+#------------Adding endpoints in Traffic Manager Profile having endpoint location as central-US------------------------------------
+
+
+
+Add-AzureRmTrafficManagerEndpointConfig -EndpointName us-endpoint -TrafficManagerProfile $Tmprofile -Type ExternalEndpoints -Target app-us.contoso.com -EndpointLocation "Central US" -EndpointStatus Enabled
+
+
+
+Set-AzureRmTrafficManagerProfile -TrafficManagerProfile $Tmprofile
+
+
+
+
+#------------Creating traffic manager profile having Routing Method as priority---------------------------------
+
+
+
+$TmprofilePri = New-AzureRmTrafficManagerProfile -Name kpTMPrio -ResourceGroupName $rgname -TrafficRoutingMethod Priority -RelativeDnsName GowthamiDns -Ttl 30 -MonitorProtocol HTTPS -MonitorPort 22 -MonitorPath "/"
+
+
+#------------Adding endpoints in Traffic Manager Profile having endpoint location as East-Us------------------------------------
+
+
+
+Add-AzureRmTrafficManagerEndpointConfig -EndpointName eu-endpoint -TrafficManagerProfile $TmprofilePri -Type ExternalEndpoints -Target app-eu.contoso.com -EndpointLocation "East US " -EndpointStatus Enabled
+#------------Adding endpoints in Traffic Manager Profile having endpoint location as central-US------------------------------------
+
+
+
+Add-AzureRmTrafficManagerEndpointConfig -EndpointName cs-endpoint -TrafficManagerProfile $TmprofilePri -Type ExternalEndpoints -Target app-us.contoso.com -EndpointLocation "Central US" -EndpointStatus Enabled
+
+
+
+Set-AzureRmTrafficManagerProfile -TrafficManagerProfile $TmprofilePri
+
+#------------Creating traffic manager profile having Routing Method as Weight---------------------------------
+
+
+
+$TmprofileWeig = New-AzureRmTrafficManagerProfile -Name kpTMWeig -ResourceGroupName $rgname -TrafficRoutingMethod Weighted -RelativeDnsName kpdns -Ttl 30 -MonitorProtocol HTTP -MonitorPort 80 -MonitorPath "/"
+
+#------------Adding endpoints in Traffic Manager Profile having endpoint location as North-europe------------------------------------
+
+
+
+Add-AzureRmTrafficManagerEndpointConfig -EndpointName eu-endpoint -TrafficManagerProfile $TmprofileWeig -Type ExternalEndpoints -Target app-eu.contoso.com -EndpointLocation "North Europe" -EndpointStatus Enabled
+
+
+
+#------------Adding endpoints in Traffic Manager Profile having endpoint location as central-US------------------------------------
+
+
+
+Add-AzureRmTrafficManagerEndpointConfig -EndpointName us-endpoint -TrafficManagerProfile $TmprofileWeig -Type ExternalEndpoints -Target app-us.contoso.com -EndpointLocation "Central US" -EndpointStatus Enabled
+
+Set-AzureRmTrafficManagerProfile -TrafficManagerProfile $TmprofileWeig
+
+
