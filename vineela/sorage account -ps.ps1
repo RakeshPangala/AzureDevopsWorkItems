e165f129b8740c7faf6b35c0252f6736136acee1
vineela4 <49679232+vineela4@users.noreply.github.com>
2019-05-14 01:59:55 -0700

    Add files via upload

---
+++ vineela/sorage account -ps.ps1
@@ -0,0 +1,24 @@
+﻿Install-Module -Name Az -AllowClobber
+
+#login Azure portal
+
+$rg = "vineela"
+$loc = "central us"
+
+#create a resource group
+
+New-AzResourceGroup -Name $rg -Location $loc
+ 
+# create a storage account
+
+$storageacc = New-AzStorageAccount -ResourceGroupName $rg -Name "vineelastorageaccount" -SkuName "Standard_LRS" -Location $loc 
+
+$ctx = $storageacc.Context
+
+#create blob container
+
+New-AzStorageContainer -Name "vineelablobcontainer" -Permission "Blob" -Context $ctx
+
+# uplode the .jpg file
+
+Set-AzStorageBlobContent -File "E:\samples\picture1.jpg" -Container "vineelablobcontainer" -Blob "picture1.jpg" -Context $ctx
