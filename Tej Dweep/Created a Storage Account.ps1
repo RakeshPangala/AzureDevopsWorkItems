07e88af8a51c8350c46bc780f58bc49b900f8172
Tej424 <49711588+Tej424@users.noreply.github.com>
2019-05-14 17:50:09 +0530

    Add files via upload

---
+++ Tej Dweep/Created a Storage Account.ps1
@@ -0,0 +1,17 @@
+﻿Login-AzAccount
+
+New-AzureRmResourceGroup -Name tej -Location SouthIndia
+
+# create a storage account
+
+$storageacc = New-AzureRmstorageaccount -ResourceGroupName tej -Name tejsa -SkuName Standard_LRS -Location SouthIndia 
+
+$ctx = $storageacc.Context
+
+#create blob container
+
+New-AzureRmStorageContainer -Name tejblobcontainer -Permission Blob -Context $ctx
+
+# uplode the .jpg file
+
+Set-AzureRmStorageBlobContent -File "D:\Tej.jpg" -Container tejblobcontainer -Blob "picture.jpg" -Context $ctx
