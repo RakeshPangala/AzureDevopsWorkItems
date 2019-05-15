82357dc5b95645426ff7e69424ec3126db4db3b1
bvkkssv1488 <49712544+bvkkssv1488@users.noreply.github.com>
2019-05-15 07:35:42 +0530

    Add files via upload

---
+++ vamshi/load balancer and scale set.ps1
@@ -0,0 +1,57 @@
+﻿Login-AzAccount
+
+#$username='quadrantit@hotmail.com'
+#$password='password'
+#$password=ConvertTo-SecureString $password -AsPlainText -Force
+#$credential= New-Object System.Management.Automation.PSCredential($username,$password)
+#Connect-AzAccount -Credential $credential
+
+
+
+
+# Common
+$LOC = "southindia"
+$RGName = "vamshi";
+
+
+
+
+$RGName = "vamshi"
+$LOC = "southindia"
+$STOName = "sto" + $RGName;
+$STOType = "Standard_GRS";
+New-AzStorageAccount -ResourceGroupName $RGName -Name $STOName -Location $LOC -Type $STOType;
+$STOAccount = Get-AzStorageAccount -ResourceGroupName $RGName -Name $STOName; 
+
+
+
+
+$SubNet = New-AzVirtualNetworkSubnetConfig -Name ("subnet" + $RGName) -AddressPrefix "10.0.0.0/24";
+$VNet = New-AzVirtualNetwork -Force -Name ("vnet" + $RGName) -ResourceGroupName $RGName -Location $LOC -AddressPrefix "10.0.0.0/16" -DnsServer "10.1.1.1" -Subnet $SubNet;
+$VNet = Get-AzVirtualNetwork -Name ('vnet' + $RGName) -ResourceGroupName $RGName;
+$SubNetId = $VNet.Subnets[0].Id;
+
+$RGName = "vamshi"
+$LOC="South India"
+$PubIP = New-AzPublicIpAddress -Force -Name ("pip1" + $RGName) -ResourceGroupName $RGName -Location $LOC -AllocationMethod Dynamic -DomainNameLabel ("pip1" + $RGName);
+$PubIP = Get-AzPublicIpAddress -Name ("pip1"  + $RGName) -ResourceGroupName $RGName;
+$FrontendName = "fe" + $RGName
+$BackendAddressPoolName = "bepool" + $RGName
+$ProbeName = "vmssprobe" + $RGName
+$InboundNatPoolName  = "innatpool" + $RGName
+$LBRuleName = "lbrule" + $RGName
+$LBName = "vmsslb" + $RGName
+
+$Frontend = New-AzLoadBalancerFrontendIpConfig -Name $FrontendName -PublicIpAddress $PubIP 
+$BackendAddressPool = New-AzLoadBalancerBackendAddressPoolConfig -Name $BackendAddressPoolName
+$Probe = New-AzLoadBalancerProbeConfig -Name $ProbeName -RequestPath healthcheck.aspx -Protocol http -Port 80 -IntervalInSeconds 15 -ProbeCount 2
+$InboundNatPool = New-AzLoadBalancerInboundNatPoolConfig -Name $InboundNatPoolName  -FrontendIPConfigurationId `
+    $Frontend.Id -Protocol Tcp -FrontendPortRangeStart 3360 -FrontendPortRangeEnd 3376 -BackendPort 3370;
+$LBRule = New-AzLoadBalancerRuleConfig -Name $LBRuleName `
+    -FrontendIPConfiguration $Frontend -BackendAddressPool $BackendAddressPool `
+    -Probe $Probe -Protocol Tcp -FrontendPort 80 -BackendPort 80 `
+    -IdleTimeoutInMinutes 15 -EnableFloatingIP -LoadDistribution SourceIP;
+$ActualLb = New-AzLoadBalancer -Name $LBName -ResourceGroupName $RGName -Location $LOC `
+    -FrontendIpConfiguration $Frontend -BackendAddressPool $BackendAddressPool `
+    -Probe $Probe -LoadBalancingRule $LBRule -InboundNatPool $InboundNatPool;
+$ExpectedLb = Get-AzLoadBalancer -Name $LBName -ResourceGroupName $RGName
