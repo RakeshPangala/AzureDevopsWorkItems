848304756b96439fec4758b4fbbfe18a6fe3f155
bvkkssv1488 <49712544+bvkkssv1488@users.noreply.github.com>
2019-05-15 07:31:28 +0530

    Add files via upload

---
+++ vamshi/Storage.ps1
@@ -0,0 +1,14 @@
+﻿Login-AzAccount
+
+
+$resourceGroupName= New-AzResourceGroup -Name "vamshirm" -Location 'South India'
+
+
+$storageAccountName= New-AzStorageAccount -ResourceGroupName "vamshirm" -Name vamshistorageacc -SkuName Standard_LRS -Location 'south india' -Kind StorageV2 -AccessTier Hot
+
+
+$context = $storageAccountName.Context
+
+$storageContainer = New-AzStorageContainer -Name "vamshicontainer" -Permission Container -Context $context
+
+Set-AzStorageBlobContent -File "C:\Users\Vamshi\Desktop\minions.jpg" -Container "container" -Blob "minions.jpg" -Context $context
