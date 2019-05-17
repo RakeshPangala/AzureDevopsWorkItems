74e87a37f152c5fff7cd3f0161b9d54090017938
GOPALRAO1 <49671757+GOPALRAO1@users.noreply.github.com>
2019-05-17 18:03:04 +0530

    Add files via upload

---
+++ Gopal/Json file modification.ps1
@@ -0,0 +1,60 @@
+﻿-------- JSON FILE-------------
+<#{
+    "update": [
+        {
+            "Name": "test1",        
+            "Version": "2.1"
+        },
+        {
+            "Name": "test2",        
+            "Version": "2.1"
+        }   
+    ]
+}
+
+#>
+
+
+----------POWERSHELL SCRIPT-------------
+
+$a = Get-Content 'F:\gopal\sample.json' -raw | ConvertFrom-Json
+$a.update | % {if($_.name -eq 'test1'){$_.version=3.0}}
+$a | ConvertTo-Json -depth 32| set-content 'F:\gopal\resample.json'
+
+
+$b=get-content 'F:\gopal\sample.json' -raw |ConvertFrom-json
+$b.update |%{if($_.name -eq 'test2'){$_.version=5.0}}
+$b | ConvertTo-Json -Depth 32| Set-Content 'F:\gopal\resampl22.json'
+
+
+------------AFTER MODIFICATION--------------
+
+<#
+ 
+{
+    "update":  [
+                   {
+                       "Name":  "test1",
+                       "Version":  3
+                   },
+                   {
+                       "Name":  "test2",
+                       "Version":  "2.1"
+                   }
+               ]
+  #>
+<#
+{
+    "update":  [
+                   {
+                       "Name":  "test1",
+                       "Version":  "2.1"
+                   },
+                   {
+                       "Name":  "test2",
+                       "Version":  5
+                   }
+               ]
+#>
+
+
