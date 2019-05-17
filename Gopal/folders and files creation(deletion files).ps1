546a5c8eb8052ae2e050c97441913d7a5835e011
GOPALRAO1 <49671757+GOPALRAO1@users.noreply.github.com>
2019-05-17 18:11:58 +0530

    Add files via upload

---
+++ Gopal/folders and files creation(deletion files).ps1
@@ -0,0 +1,34 @@
+﻿#----------------------Creates a Folder------------------------------#
+New-Item -Path 'F:\' -ItemType Directory -Name 'GOPAL'
+
+#----------Creates a SubFolder inside a Folder-----------------------#
+
+New-Item -Path 'F:\GOPAL' -ItemType Directory -Name KARNA
+
+#-----------Creates a Folder inside a Folder-------------------------#
+
+New-Item -Path 'F:\GOPAL' -ItemType Directory -Name ARJUNA
+
+#----------------------Creates a File--------------------------------#
+
+New-Item -Path 'F:\GOPAL\ARJUNA\Text.Txt' -ItemType File
+
+#----------------------Creates a Folder------------------------------#
+
+New-Item -Path 'F:\GOPAL' -ItemType Directory -Name BHEEMA
+
+
+
+#------------Deleting files------------  #
+
+
+$FileName = 'F:\GOPAL'
+
+do {
+
+       $dir = gci $FileName -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
+
+       $dir | Foreach-Object { Remove-Item $_ }
+
+    } while ($dir.count -gt 0)
+
