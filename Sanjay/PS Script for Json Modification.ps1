9cb45b8bf442e27a1a60fd2719e8176265c84d01
Sanjay <46832361+Sanjay3761@users.noreply.github.com>
2019-05-17 18:25:59 +0530

    Add files via upload

---
+++ Sanjay/PS Script for Json Modification.ps1
@@ -0,0 +1,3 @@
+﻿$a = Get-Content 'C:\Users\SANJAY\Desktop\MyJson.json' -raw | ConvertFrom-Json
+$a.update | % {if($_.name -eq 'test1'){$_.version=4.0}}
+$a | ConvertTo-Json -depth 32| set-content 'C:\Users\SANJAY\DESKTOP\MyJson1.json'
