6cb823509f7457070a55d8975ea2f3773bd1cdbf
Sai Krishna <46664894+KrishReigns@users.noreply.github.com>
2019-05-18 21:57:09 +0530

    Powershell script for json modification

---
+++ Sai Krishna/JsonAbbrevationChange.ps1
@@ -0,0 +1,3 @@
+﻿$Json = Get-Content 'C:\Users\v-saikka\Sample1.json' -raw | ConvertFrom-Json
+$Json.update | % {if($_.Name -eq 'Krishna Kumar'){$_.Abbrevation='KK'}}
+$Json | ConvertTo-Json -depth 32| set-content 'C:\Users\v-saikka\Sample1.json'
