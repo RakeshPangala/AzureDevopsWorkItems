928c30505c54cd99eb30872510309928a3b4a271
B121215 <46589065+B121215@users.noreply.github.com>
2019-05-19 23:22:44 +0530

    Add files via upload

---
+++ Gowthami/JsonmodifiyingFile.ps1
@@ -0,0 +1,87 @@
+﻿#Before modifiying Json file
+{
+    "update":  [
+                   {
+                       "Name":  "GowthamiSample",
+                    "Version": "2.1",
+                    "Abbrevation": "G"
+                   },
+                   {
+                       "Name":  "Gowthami",
+                    "Version": "6.0",
+                      "Abbrevation": "G"
+                   },
+                   {
+                       "Name":  "Potharju",
+                    "Version": "2.3",
+                    "Abbrevation": "P"
+                   },
+                   {
+                       "Name":  "Potharaju Gowthami",
+                    "Version": "2.4",
+                    "Abbrevation": "P"
+                   },
+                   {
+                       "Name":  "Session",
+                    "Version": "2.5",
+                    "Abbrevation": "S"
+                   },
+                   {
+                       "Name":  "Divya",
+                    "Version": "2.6",
+                    "Abbrevation": "D"
+                   }
+               ]
+}
+
+
+#Powershell script for midifying json file
+
+$Json = Get-Content 'C:\Users\Gowthami\GowthamiSample1.json' -raw | ConvertFrom-Json
+$Json.update | % {if($_.Name -eq 'Potharaju Gowthami'){$_.Abbrevation='PG'}}
+$Json | ConvertTo-Json -depth 32| set-content 'C:\Users\Gowthami\GowthamiSample1.json'
+
+
+#After modifying json file 
+
+{
+    "update":  [
+                   {
+                       "Name":  "GowthamiSample",
+                       "Version":  "2.1",
+                       "Abbrevation":  "S"
+                   },
+                   {
+                       "Name":  "Gowthami",
+                       "Version":  "6.0",
+                       "Abbrevation":  "G"
+                   },
+                   {
+                       "Name":  "Potharaju",
+                       "Version":  "2.3"
+                       "Abbrevation":  "P"
+                   },
+                   {
+                       "Name":  "Potharaju Gowthami",
+                       "Version":  "2.4",
+                       "Abbrevation":  "PG"
+                   },
+                   {
+                       "Name":  "Session",
+                       "Version":  "2.5",
+                       "Abbrevation":  "S"
+                   },
+                   {
+                       "Name":  "Divya",
+                       "Version":  "2.6",
+                       "Abbrevation":  "D"
+                   }
+               ]
+}
+
+#Json Abbriviation change
+
+
+$Json = Get-Content 'C:\Users\Gowthami\GowthamiSample1.json' -raw | ConvertFrom-Json
+$Json.update | % {if($_.Name -eq 'Potharaju Gowthami'){$_.Abbrevation='PG'}}
+$Json | ConvertTo-Json -depth 32| set-content 'C:\Users\Gowthami\GowthamiSample1.json'
---
+++ Gowthami/findinglargestnumber.ps1
@@ -0,0 +1,15 @@
+﻿
+$maxValue = function arrayTest {
+$array=@(8,4,2,9,1,4,7)
+        $mxm = array[0];
+       for ($i=0; $i -le $array.length;$i++) {
+        if ($array[$i] -gt $mxm) {
+          $mxm = $array[$i];
+          $array.splice($array.indexOf($array.max), 1); 
+                }
+             }
+           #return $mxm;
+           return $mxm;
+            
+};
+arrayTest
