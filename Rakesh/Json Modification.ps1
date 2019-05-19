e203e315a2e158cce3e5b03e8cfc68c00c9ac808
RakeshPangala <46561538+RakeshPangala@users.noreply.github.com>
2019-05-19 11:22:11 +0530

    Add files via upload

---
+++ Rakesh/Json Modification.ps1
@@ -0,0 +1,78 @@
+﻿#This is Json file
+
+
+
+{
+
+    "update": [
+
+        {
+
+            "Name": "Rakesh1",        
+
+            "Version": "2.1"
+
+        },
+
+        {
+
+            "Name": "Rakesh2",        
+
+            "Version": "2.1"
+
+        }   
+
+    ]
+
+}
+
+
+
+
+
+#Updating this Json file using Powershell
+
+
+
+$a = Get-Content 'C:\Users\Rakesh\Desktop\MyJson.json' -raw | ConvertFrom-Json
+
+$a.update | % {if($_.name -eq 'Rakesh1'){$_.version=3.0}}
+
+$a | ConvertTo-Json -depth 32| set-content 'C:\Users\Rakesh\DESKTOP\MyJson1.json'
+
+
+
+
+
+
+
+#After modifying 
+
+
+
+
+
+{
+
+    "update":  [
+
+                   {
+
+                       "Name":  "Rakesh1",
+
+                       "Version":  3
+
+                   },
+
+                   {
+
+                       "Name":  "Rakesh2",
+
+                       "Version":  "2.1"
+
+                   }
+
+               ]
+
+}
+
