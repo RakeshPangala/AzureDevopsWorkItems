152e6f484c101b6477dd824361a11f87ceb079dc
ghorpadeanup <49711541+ghorpadeanup@users.noreply.github.com>
2019-05-20 09:18:26 +0530

    Add files via upload

---
+++ Anup/ModifyValueJson.ps1
@@ -0,0 +1,5 @@
+﻿$Json = Get-Content 'C:\Users\v-anugho\Pictures\Azure\ModifyValue.json' -raw | ConvertFrom-Json
+
+$Json.update | % {if($_.Name -eq 'WebConfigFile'){$_.Abbrevation='WF'}}
+
+$Json | ConvertTo-Json -depth 32| set-content 'C:\Users\v-anugho\Pictures\Azure\ModifyValue.json'
