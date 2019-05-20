9a995ad2d8bdf18fce02e5e203a1c85195a70c1d
cpsai <49713233+cpsai@users.noreply.github.com>
2019-05-20 09:28:04 +0530

    Add files via upload

---
+++ pratap/json.ps1
@@ -0,0 +1,41 @@
+#This is Json file
+
+{
+    "update": [
+        {
+            "Name": "test1",        
+            "Version": "2.1"
+        },
+        {
+            "Name": "test2",        
+            "Version": "2.1"
+        }   
+    ]
+}
+
+
+#Updating this Json file using Powershell
+
+$a = Get-Content 'C:\Users\SANJAY\Desktop\MyJson.json' -raw | ConvertFrom-Json
+$a.update | % {if($_.name -eq 'test1'){$_.version=4.0}}
+$a | ConvertTo-Json -depth 32| set-content 'C:\Users\SANJAY\DESKTOP\MyJson1.json'
+
+
+
+#After modifying 
+
+
+{
+    "update":  [
+                   {
+                       "Name":  "test1",
+                       "Version":  4
+                   },
+                   {
+                       "Name":  "test2",
+                       "Version":  "2.1"
+                   }
+               ]
+}
+
+
