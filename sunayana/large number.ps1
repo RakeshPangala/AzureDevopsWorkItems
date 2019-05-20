856b013899ac55ef4da7d2b91deb6a2d2140ed36
surya8899 <49714137+surya8899@users.noreply.github.com>
2019-05-20 20:33:21 +0530

    Add files via upload

---
+++ sunayana/large number.ps1
@@ -0,0 +1,21 @@
+﻿$mylist=11,555,3,41,5,6
+
+$first=$second=0
+
+$i=0
+
+for($i=0;$i-le $mylist.Count;$i++)
+{
+
+if($mylist[$i] -gt $first)
+
+{
+$second=$first
+
+$first=$mylist[$i]
+
+}
+
+}
+
+Write-Host("The First and Second largest Numbers in an Array is:"+($first,$second))
