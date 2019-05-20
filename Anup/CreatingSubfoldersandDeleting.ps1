f05887f78fa156116456f7cca5b2e88750116906
ghorpadeanup <49711541+ghorpadeanup@users.noreply.github.com>
2019-05-20 09:39:53 +0530

    Add files via upload

---
+++ Anup/CreatingSubfoldersandDeleting.ps1
@@ -0,0 +1,24 @@
+﻿#----------------------Creates a Folder------------------------------#
+
+New-Item -Path 'C:\Users\v-anugho\Pictures\Azure\create and delete' -ItemType Directory -Name 'MainFolder'
+
+#----------Creates a SubFolder inside a Folder-----------------------#
+
+New-Item -Path 'C:\Users\v-anugho\Pictures\Azure\create and delete\MainFolder' -ItemType Directory -Name SubFolder1
+
+#-----------Creates a Folder inside a Folder-------------------------#
+
+New-Item -Path 'C:\Users\v-anugho\Pictures\Azure\create and delete\MainFolder' -ItemType Directory -Name SubFolder2
+
+#----------------------Creates a File--------------------------------#
+
+New-Item -Path 'C:\Users\v-anugho\Pictures\Azure\create and delete\MainFolder\SubFolder2\text.txt' -ItemType File
+
+#----------------------Creates a Folder------------------------------#
+
+New-Item -Path 'C:\Users\v-anugho\Pictures\Azure\create and delete\MainFolder' -ItemType Directory -Name SubFolder3
+
+
+#----------------------Deleting a Folder------------------------------#
+
+
