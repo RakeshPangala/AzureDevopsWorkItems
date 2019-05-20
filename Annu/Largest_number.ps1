c70f122fc11e940ebb601a67f2e8a18cec53c9c5
AnnuCh <46590008+AnnuCh@users.noreply.github.com>
2019-05-20 09:23:10 +0530

    Add files via upload

---
+++ Annu/Largest_number.ps1
@@ -0,0 +1,13 @@
+﻿$array=@(3,6,5,1,7,9,1)
+$array
+
+$x=$array|Measure-Object -Maximum
+$x
+$y=$x|Where-Object |Select-Object -Property "maximum" -ExpandProperty Maximum
+$y
+
+
+$z=$x|Where-Object |Select-Object -Property "maximum" 
+
+$z
+
