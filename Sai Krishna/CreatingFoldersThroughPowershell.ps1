8ed9db39cacc4e22b96611f9bffb11080a790f69
Sai Krishna <46664894+KrishReigns@users.noreply.github.com>
2019-05-20 07:42:43 +0530

    Added file that creates folders and subfolders

---
+++ Sai Krishna/CreatingFoldersThroughPowershell.ps1
@@ -0,0 +1,28 @@
+﻿#Creating three empty folders
+
+New-Item -Path 'C:\krish1' -ItemType Directory
+New-Item -Path 'C:\krish2' -ItemType Directory
+New-Item -Path 'C:\krish3' -ItemType Directory
+
+# Get folders that are krish1,krish2,krish3
+
+$Folders = Get-ChildItem 'C:\*' | Where-Object -Property Name -in -Value 'krish1','krish2','krish3'
+
+
+ 
+
+
+# Loop through those folders
+
+foreach ($Folder in $Folders)
+
+{
+
+    $2017Path = Join-Path -Path $Folder.FullName -ChildPath '2017' # Generate path to 2017 folder
+
+    $2018Path = Join-Path -Path $Folder.FullName -ChildPath '2018' # Generate path to 2018 folder
+
+    New-Item -Path $2017Path -Force # Create 2017 folder
+
+    New-Item -Path $2018Path -Force # Create 2018 folder
+}
