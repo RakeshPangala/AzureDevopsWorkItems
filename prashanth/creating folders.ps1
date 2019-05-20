d6efb21af5615860d19b9a7fef7babe3e1d30482
kPrashanth007 <49711439+kPrashanth007@users.noreply.github.com>
2019-05-20 09:37:15 +0530

    Add files via upload

---
+++ prashanth/creating folders.ps1
@@ -0,0 +1,13 @@
+﻿ $files = ('aa','bb','cc','dd')
+foreach($file in $files)
+{
+    New-Item -ItemType Directory $files 
+    new-item -ItemType file $file\'test.txt' 
+    Add-Content $file\test.txt 'this is the test content in the file.'
+}
+
+foreach ($file in $files) {
+    if($file -ne 'aa'){
+    Remove-Item $file
+    }
+}
