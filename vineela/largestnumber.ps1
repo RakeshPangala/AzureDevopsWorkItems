6597307f9ef5c86f1a1270d76b11cfaf2f134fcf
vineela4 <49679232+vineela4@users.noreply.github.com>
2019-05-19 21:08:47 -0700

    Add files via upload

---
+++ vineela/largestnumber.ps1
@@ -0,0 +1,21 @@
+﻿$mylist=5,4,2,6,3,1
+
+$first=$second=0
+
+$i=0
+
+for($i=0;$i-le $mylist.Count;$i++)
+{
+
+if($mylist[$i] -gt $first)
+
+{
+$second=$first
+
+$first=$mylist[$i]
+
+}
+
+}
+
+Write-Host("The First and Second Highest Numbers in an Array is:"+($first,$second))
