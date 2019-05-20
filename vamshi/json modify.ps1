ca4aa8490a66f1426d4cd775face4448fe1b97bf
bvkkssv1488 <49712544+bvkkssv1488@users.noreply.github.com>
2019-05-20 09:32:51 +0530

    Add files via upload

---
+++ vamshi/json modify.ps1
@@ -0,0 +1,4 @@
+﻿$a = Get-Content 'c:\vamshi.json' -raw | ConvertFrom-Json
+$a.employee | % {if($_.name -eq 'sonoo'){$_.married='false'}}
+$a | ConvertTo-Json -depth 32| set-content 'c:\vamshi.json'
+
---
+++ vamshi/second highest.ps1
@@ -0,0 +1,17 @@
+﻿$arr=3,4,2,5,8
+for($i=0;$i-le $arr.Length-1 ;$i++){
+for($j=1;$j-le $arr.length;$j++){
+if($arr[$i]-gt $arr[$j]){
+$hold=$i
+$arr[$i]=$arr[$j]
+$arr[$j]=$hold
+}
+}
+}
+echo $arr[1]
+
+
+
+
+
+
