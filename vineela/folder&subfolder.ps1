7d465dd80d25578f00c44b015a9cf07c9d752a83
vineela4 <49679232+vineela4@users.noreply.github.com>
2019-05-19 19:29:49 -0700

    Add files via upload

---
+++ vineela/folder&subfolder.ps1
@@ -0,0 +1,20 @@
+﻿#Creates a Folder
+New-Item -Path 'D:\' -ItemType Directory -Name 'PowerShell'
+
+#Creates a SubFolder inside a Folder
+
+New-Item -Path 'D:\PowerShell' -ItemType Directory -Name Subfolder1
+
+#Creates a Folder inside a Folder
+
+New-Item -Path 'D:\PowerShell' -ItemType Directory -Name Subfolder2
+
+#Creates a File
+
+New-Item -Path 'D:\PowerShell\SubFolder2\Text.Txt' -ItemType File
+
+#Creates a Folder
+
+New-Item -Path 'D:\PowerShell' -ItemType Directory -Name Subfolder3
+
+
