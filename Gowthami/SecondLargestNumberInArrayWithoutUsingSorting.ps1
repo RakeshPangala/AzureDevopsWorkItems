ca18b0df8358b416f178217038667bfca4d7fa15
B121215 <46589065+B121215@users.noreply.github.com>
2019-05-20 09:11:58 +0530

    Add files via upload

---
+++ Gowthami/SecondLargestNumberInArrayWithoutUsingSorting.ps1
@@ -0,0 +1,16 @@
+﻿$array=@(3,6,5,1,7,9,1)
+$array
+
+$a=$array|Measure-Object -Maximum
+$a
+$b=$a|Where-Object |Select-Object -Property "maximum" -ExpandProperty Maximum
+$b
+
+
+$c=$a|Where-Object |Select-Object -Property "maximum" 
+$c
+
+$d=$array.slice($array.indexOf(max), 1); // remove max from the array
+$d
+$output=Math.max.apply(null, arr); // get the 2nd max
+$output
