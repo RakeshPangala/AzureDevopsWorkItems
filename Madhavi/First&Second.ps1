813fd77409f41e57653380aa51d92a4eeee772ac
MadhaviKonthalapally <48505956+MadhaviKonthalapally@users.noreply.github.com>
2019-05-20 09:41:23 +0530

    Add files via upload

---
+++ Madhavi/First&Second.ps1
@@ -0,0 +1,23 @@
+﻿
+$mylist=6,8,1,2,3
+
+$first=$second=0
+
+for($i=0;$i-le $mylist.Count;$i++)
+
+{
+
+
+if($mylist[$i] -gt $first)
+
+{
+
+$second=$first
+
+$first=$mylist[$i]
+
+}
+
+}
+
+Write-Host("The First and Second Highest Numbers in an Array is:"+($first,$second))
