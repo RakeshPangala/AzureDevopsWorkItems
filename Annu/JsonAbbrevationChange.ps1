ace0404de8d6295ed3fe54a10b7373b6cc6c6d47
AnnuCh <46590008+AnnuCh@users.noreply.github.com>
2019-05-20 09:28:20 +0530

    Add files via upload

---
+++ Annu/JsonAbbrevationChange.ps1
@@ -0,0 +1,3 @@
+﻿$Json = Get-Content 'C:\Sample1.json' -raw | ConvertFrom-Json
+$Json.update | % {if($_.Name -eq 'Java script Object Notation'){$_.Abbrevation='JSON'}}
+$Json | ConvertTo-Json -depth 32| set-content 'C:\Sample1.json'
---
+++ Annu/WebConfigPowershell.ps1
@@ -0,0 +1,8 @@
+﻿$xml = [xml](Get-Content c:\Web.config)
+$String = $xml.connectionStrings.add[0].connectionString
+$String2 = $String -replace '10.10.10.10','172.11.0.10'
+$xml.connectionStrings.add[0].connectionString = $String2
+$String = $xml.connectionStrings.add[1].connectionString
+$String2 = $String -replace '10.10.10.10','172.11.0.10'
+$xml.connectionStrings.add[1].connectionString = $String2
+$xml.Save('c:\Web2.config')
