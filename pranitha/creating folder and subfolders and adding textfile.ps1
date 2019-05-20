7f7a49c7f67a6c14c48cd4c8512675d3bb0b12c9
pranitha112 <49674063+pranitha112@users.noreply.github.com>
2019-05-20 07:12:40 +0530

    Add files via upload

---
+++ pranitha/creating folder and subfolders and adding textfile.ps1
@@ -0,0 +1,17 @@
+﻿#creating folder
+New-Item -Path 'D:\temp\Folder' -ItemType Directory
+#creating subfolder1 inside folder
+New-Item -Path 'D:\temp\Folder\subfolder1' -ItemType Directory 
+#creating subfolder2 inside folder
+New-Item -Path 'D:\temp\Folder\subfolder2' -ItemType Directory 
+#creating subfolder3 inside folder
+New-Item -Path 'D:\temp\Folder\subfolder3' -ItemType Directory 
+
+
+
+
+
+#creating Newfile in subfolder3 and adding text in file
+New-Item -Path 'D:\temp\Folder\subfolder3\Test File.txt' -ItemType File
+Set-Content D:\temp\test\test.txt 'Hello'
+Get-Content D:\temp\test\test.txt
