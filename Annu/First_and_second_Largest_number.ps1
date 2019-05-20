eb41233967580a36b751e0e61a6f86566cac3674
AnnuCh <46590008+AnnuCh@users.noreply.github.com>
2019-05-20 09:35:04 +0530

    Add files via upload

---
+++ Annu/First_and_second_Largest_number.ps1
@@ -0,0 +1,21 @@
+﻿$mylist=4,2,7,8,5,1
+
+$first=$second=0
+
+$i=0
+
+for($i=0;$i-le $mylist.Count;$i++)
+{
+
+if($mylist[$i] -gt $first)
+
+{
+$second=$first
+
+$first=$mylist[$i]
+
+}
+
+}
+
+Write-Host("The First and Second Highest Numbers in an Array is:"+($first,$second))
