71344a10af03adb4a1cf63c2c2ad758d4c63e156
pranitha112 <49674063+pranitha112@users.noreply.github.com>
2019-05-20 06:38:32 +0530

    Add files via upload

---
+++ pranitha/First highest and second highest number.ps1
@@ -0,0 +1,24 @@
+﻿
+#To find first highest and second highest number
+
+$a=3,5
+$i=0
+if($a[$i]-gt$i)
+{
+$first=$a[1];
+$second=$a[0];
+}
+else
+{
+
+$first=$a[0];
+$second=$a[1];}
+
+Write-Host("The First and Second Highest Numbers in an Array is:"+($first,$second))
+
+# To find highest number
+$a = 242
+$b = 12
+$c = 31
+$d = 24
+($a,$b,$c,$d|Measure-Object -Maximum).Maximum
