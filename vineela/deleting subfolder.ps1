c6403b1bffd84dc997a4316a3a9b7728a18dfce4
vineela4 <49679232+vineela4@users.noreply.github.com>
2019-05-19 23:29:25 -0700

    Delete deleting subfolder.ps1

--- vineela/deleting subfolder.ps1
+++
@@ -1,9 +0,0 @@
-﻿$FileName = 'D:\PowerShell'
-
-do {
-
-       $dir = gci $FileName -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
-
-       $dir | Foreach-Object { Remove-Item $_ }
-
-    } while ($dir.count -gt 0)
