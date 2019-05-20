b30fbfb2a13d3797e72c0627b1cc137b57cde753
pranitha112 <49674063+pranitha112@users.noreply.github.com>
2019-05-19 23:26:40 -0700

    Add files via upload

---
+++ pranitha/find first and second high from list.ps1
@@ -0,0 +1,12 @@
+﻿$mylist=13,10,17,19,23
+$first=$second=0
+$i=0
+for($i=0;$i-le $mylist.Count;$i++)
+{
+if($mylist[$i] -gt $first)
+{
+$second=$first
+$first=$mylist[$i]
+}
+}
+Write-Host("The First and Second Highest Numbers in an Array is:"+($first,$second))
