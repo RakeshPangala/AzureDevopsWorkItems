5d911317b83d71c8a0bd25f034232f53c76b707f
cpsai <49713233+cpsai@users.noreply.github.com>
2019-05-20 09:30:28 +0530

    Update json.ps1

--- pratap/json.ps1
+++ pratap/json.ps1
@@ -16,7 +16,7 @@
 
 #Updating this Json file using Powershell
 
-$a = Get-Content 'C:\Users\SANJAY\Desktop\MyJson.json' -raw | ConvertFrom-Json
+$a = Get-Content 'C:\Users\v-saipc\Desktop\json.json' -raw | ConvertFrom-Json
 $a.update | % {if($_.name -eq 'test1'){$_.version=4.0}}
 $a | ConvertTo-Json -depth 32| set-content 'C:\Users\SANJAY\DESKTOP\MyJson1.json'
 
