5a0ab6e701b48f669583c9f73a870245dd379682
Sai Krishna <46664894+KrishReigns@users.noreply.github.com>
2019-05-20 09:37:28 +0530

    Delete Empty Folders

---
+++ Sai Krishna/ScriptDeletesEmptyFolders.ps1
@@ -0,0 +1,18 @@
+﻿
+$Path= 'C:\Krish'
+
+
+
+do {
+
+
+
+       $directory = gci $Path -directory -recurse | Where { (gci $_.fullName).count -eq 0 } | select -expandproperty FullName
+
+
+
+       $directory | Foreach-Object { Remove-Item $_ }
+
+
+
+    } while ($directory.count -gt 0)
