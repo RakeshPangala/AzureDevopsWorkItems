df06776e59584c0d5a46bc2db256a0a7b5b4694a
VaijayanthiS <46589969+VaijayanthiS@users.noreply.github.com>
2019-05-20 09:30:05 +0530

    Add files via upload

---
+++ Vaijayanthi/FirstLargestAndSecondLargestNo.ps1
@@ -0,0 +1,21 @@
+﻿$mylist=1,2,3,4,5,6
+
+$first=$second=0
+
+$i=0
+
+for($i=0;$i-le $mylist.Count;$i++)
+{
+
+if($mylist[$i] -gt $first)
+
+{
+$second=$first
+
+$first=$mylist[$i]
+
+}
+
+}
+
+Write-Host("The First and Second Highest Numbers in an Array is:"+($first,$second))
