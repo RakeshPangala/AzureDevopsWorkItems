9f9b1043cbc0ebdd4b37c1bf3eb9527952262313
Sai Krishna <46664894+KrishReigns@users.noreply.github.com>
2019-05-20 08:57:51 +0530

    Web config file ps script

---
+++ Sai Krishna/Webdotconfig.ps1
@@ -0,0 +1,8 @@
+﻿$xml = [xml](Get-Content c:\Users\v-saikka\Web.config)
+$conString = $xml.connectionStrings.add[0].connectionString
+$conString2 = $conString -replace '192.168.1.100','10.10.10.10'
+$xml.connectionStrings.add[0].connectionString = $conString2
+$conString = $xml.connectionStrings.add[1].connectionString
+$conString2 = $conString -replace '192.168.1.100','10.10.10.10'
+$xml.connectionStrings.add[1].connectionString = $conString2
+$xml.Save('c:\Users\v-saikka\Web2.config')
