7198256342ecbc48b65f7a88ac28c4f9bbec6b2a
vineela4 <49679232+vineela4@users.noreply.github.com>
2019-05-19 23:22:00 -0700

    Delete json after modification.ps1

--- json after modification.ps1
+++
@@ -1,3 +0,0 @@
-﻿$a = Get-Content 'C:\Users\vineela\Desktop\MyJson.json' -raw | ConvertFrom-Json
-$a.update | % {if($_.name -eq 'test1'){$_.version=4.0}}
-$a | ConvertTo-Json -depth 32| set-content 'C:\Users\vineela\DESKTOP\MyJson1.json'
