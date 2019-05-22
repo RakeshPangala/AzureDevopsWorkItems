81c64b7305b8cdfb7f6318e9c864531443ab96d1
vineela4 <49679232+vineela4@users.noreply.github.com>
2019-05-22 13:01:57 +0530

    Add files via upload

---
+++ vineela/create sql database.ps1
@@ -0,0 +1,20 @@
+﻿Install-Module -Name Az -AllowClobber
+
+#create resource group
+
+ New-AzResourceGroup -Name "vineela" -Location "south india"
+
+$databasename = "vineeladatabase"
+$servername = "vineelaserver"
+
+#create sql server
+
+$server = New-AzSqlServer -ServerName $servername -ResourceGroupName "vineela" -Location "south india" -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "sqladmin",$(ConvertTo-SecureString -String Chvineela504* -AsPlainText -Force))
+
+#create sql database
+
+$database = New-AzSqlDatabase -DatabaseName $databasename -ResourceGroupName "vineela" -ServerName $servername -SampleName AdventureWorksLT 
+
+#set firewall settings
+
+$serverfirewall = New-AzSqlServerFirewallRule -FirewallRuleName "vineelarule" -ResourceGroupName "vineela" -StartIpAddress "192.168.145.65" -EndIpAddress "192.168.145.65" -ServerName $servername 
